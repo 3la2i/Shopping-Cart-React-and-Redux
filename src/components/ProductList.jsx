ff1d50270e8f59caa245d27f7da816e00176b82c
3la2i <alaa.ata25@gmail.com>
2024-09-12 09:30:06 +0300

    task

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,15 +1,9 @@
 
 import React from 'react';
-// import { useDispatch } from 'react-redux';
-// import { addToCart } from '../store/cartSlice';
+
 import { food_list } from '../assets/assets';
 
 const ProductList = () => {
-  // const dispatch = useDispatch();
-
-  // const handleAddToCart = (product) => {
-  //   dispatch(addToCart(product));
-  // };
 
   return (
     <div className="grid  grid-cols-3 gap-4">
